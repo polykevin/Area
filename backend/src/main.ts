95e980da34b0da9cb6784448d2e8c35f8ee00346
lisaglmt <lisa.goulmot@epitech.eu>
2025-12-02 18:30:48 +0100

    feat: enable global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: "*"});
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+
   await app.listen(8080);
 }
 bootstrap();
